c206afc7e2bd368a66d4b61dd9fdc0fe44610297
Andrew <andrew.fedosseyenko@gmail.com>
2024-03-05 16:22:24 +0600

    Made decimal>binary / binary>decimal converter

--- Udemy Course/Final Exercises/Int's.py
+++ Udemy Course/Final Exercises/Int's.py
@@ -46,7 +46,46 @@ primenumbers()
 
 # Возврат сдачи** - Пользователь вводит цену товара, и сколько денег дал кассиру покупатель. Программа рассчитывает, сколько каких купюр и монет следует отдать в виде сдачи.
 
-# Перевод бинарных чисел в десятичные и обратно** - Напишите программу, которая переводит десятичное число в бинарное, или бинарное число в десятичное.
+def Converter():# Перевод бинарных чисел в десятичные и обратно** - Напишите программу, которая переводит десятичное число в бинарное, или бинарное число в десятичное.
+    binary_to_decimal = None
+    answer = input ('Требуется перевод из десятичной системы в двоичную? ')
+    if answer.lower() == 'да':
+        binary_to_decimal = False
+        number = input('Введите число: ')
+    else:
+        binary_to_decimal = True
+        number = input('Введите число: ')
+
+    def binary_decimal(binary_to_decimal, number):
+        answer = ''
+        if binary_to_decimal == False: # Десятичное число в бинарное
+            number = int(number)
+            print ('Десятичное число', number, 'в бинарное')
+            while number > 1:
+                number = int(number)
+                number = number / 2
+                if number.is_integer():
+                    answer += '0'
+                    continue
+                else:
+                    answer += '1'
+                    continue
+            else:
+                print(answer[::-1])
+        else: # Бинарное в десятичное 
+            answer = 0
+            print ('Бинарное число ', number,'в десятичное')
+            number_list = list(number)
+            count = len(number_list)-1
+            print (count)
+            print (number_list)
+            for item in number_list:
+                answer += int(item) * (2**count)
+                count -= 1
+                continue
+            print (answer)
+    binary_decimal(binary_to_decimal, number)
+Converter()
 
 # Калькулятор** - Простой калькулятор для выполнения простых действий. Для более сложного задания, можете реализовать научный калькулятор.
 
