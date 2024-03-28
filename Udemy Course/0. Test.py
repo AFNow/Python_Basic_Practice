bfb88b66101b7c2bd9096d6db928db51ef80d6e7
Andrew <andrew.fedosseyenko@gmail.com>
2024-03-28 14:11:04 +0600

    Experimenting with the memory location of integer data

--- Udemy Course/0. Test.py
+++ Udemy Course/0. Test.py
@@ -1,9 +1,8 @@
-list1 = [1,2,3,4,5]
-list2 = [1,2,3,4,5]
-list1id = id(list1)
-print (id(list1))
-list2id = id(list2)
-print (id(list2))
-print (id(list1) == id(list2))
-list1.append(6)
-print (int(id(list1)) == list1id)
+def sum_a_b(a,b):
+    a += 1
+    c = a+b
+    return c
+
+num1 = 10
+num2 = 5
+print (sum_a_b(num1, num2))
