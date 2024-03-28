177225502bc468b3be8a31abc8407f896ebf1f2b
Andrew <andrew.fedosseyenko@gmail.com>
2024-03-28 14:21:05 +0600

    Made some experiments

--- Udemy Course/0. Test.py
+++ Udemy Course/0. Test.py
@@ -1,8 +1,14 @@
 def sum_a_b(a,b):
+    print (id (a))
     a += 1
+    print (id(a))
+    print (id(b))
     c = a+b
+    print (id(c))
     return c
 
 num1 = 10
 num2 = 5
 print (sum_a_b(num1, num2))
+print (id(num1))
+print (id(num2))
