b58e4cb1cc9a811ca345565e11d550eb2eaa2415
Andrew <andrew.fedosseyenko@gmail.com>
2024-04-02 14:27:23 +0600

    Completed example

--- Udemy Course/0. Test.py
+++ Udemy Course/0. Test.py
@@ -1,9 +1,14 @@
-import time
-def count1m (num):
-    start = time.time()
-    while num < 1_000_000_000:
-        num += 1
-    end = time.time()
-    print (num)
-    print (f"Время выполнения :{end - start}")
-count1m (0)
+info = {
+    'distance' : 0,
+    'speed' : 5,
+    'time' : 3
+}
+def route_info(info):
+    if ('distance' in info) and (type(info['distance']) != float) and (info['distance'] != 0):
+        print (f"The distance is {info['distance']} kilometers")
+    elif info.get('time') and info.get('speed'):
+        print (f"The distance is {info['speed'] * info['time']} kilometers")
+    else:
+        print ('No info available')
+
+route_info(info)
