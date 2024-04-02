44a5e2dcbe9c37944f01fde00395999e8f6b469e
Andrew <andrew.fedosseyenko@gmail.com>
2024-04-02 16:59:33 +0600

    Few little examples

--- Udemy Course/0. Test.py
+++ Udemy Course/0. Test.py
@@ -1,2 +1,28 @@
-image_res = ('1920', '1080')
-print (f"{image_res[0]} x {image_res[1]}") if len (image_res) == 2 else print ('Incorrect resolution')
+def dict_to_list():
+    dictionary = {
+    'name' : 'Dave',
+    'id'   : 6203,
+    'num'  : 73552,
+    1      : True
+    }
+    lst = []
+    for item in dictionary.items():
+        key, value = item
+        if type(key) == int:
+            key = key * 2
+        st = (key, value)
+        lst.append(st)
+    print (lst)
+dict_to_list()
+
+
+def filter_list(lst, data_type):
+    #end_lst = [item for item in lst if type(item) == data_type] # Example 1 in one codeline
+    end_lst = []
+    for item in lst:
+        if type(item) == data_type:
+            end_lst.append(item)  
+    print (end_lst)
+lst = [35, 11.2, True, False, 'Id', 22, 'name', 22.2]
+filter_list(lst, data_type=bool)
+
