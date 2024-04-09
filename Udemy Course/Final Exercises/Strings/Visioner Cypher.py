e212aa12e61fcbc9b4d849187e325d5ca4c85bbf
Andrew <andrew.fedosseyenko@gmail.com>
2024-04-09 16:51:18 +0600

    Started the visioner cypher exercise

--- Udemy Course/Final Exercises/Strings/Visioner Cypher.py
+++ Udemy Course/Final Exercises/Strings/Visioner Cypher.py
@@ -1,1 +1,22 @@
 # Шифр Виженера - Функции для зашифровывания и расшифровывания сообщений.
+def VisionerCypher():
+    def CypherOperandCheck():
+        kirillic_alphabeth = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+        kirillic_alphabeth_dict = {k : v for k, v in enumerate(kirillic_alphabeth.lower())}
+        latin_alphabeth = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        latin_alphabeth_dict = {k : v for k, v in enumerate(latin_alphabeth.lower())}
+        cypher = input ('Введите шифруемую строку: ')
+        for item in cypher.lower()[0]:
+            if item in kirillic_alphabeth_dict.values():
+                print ("Используется кириллический алфавит")
+                CypherPreparing()
+            elif item in latin_alphabeth_dict.values():
+                print("Используется латинский алфавит")
+                CypherPreparing()
+    CypherOperandCheck()
+
+
+    def CypherPreparing():
+        pass
+
+# https://inventwithpython.com/hacking/chapter19.html
