f2af3cedf9e578a5c3c5132c5495f1980f2d0368
Andrew <andrew.fedosseyenko@gmail.com>
2024-04-19 19:17:25 +0500

    Added the vowel count exercise

---
+++ CodeAbbey/Vowel Count.py
@@ -0,0 +1,20 @@
+input = ['fnzdxccxdkpunjqsmnmpvdk xzr  eyjgpkzvmku nuszpy',
+        'evucqjfazjinpsfplbfcehincxbskwzm ppgscr    ewrb',
+        'rrjsjbm aruemeiszqzcu izazs j mzrofkqpfxzc immjradga ktp',
+        'ugcrelnfaqmytkospnqarrd krcnncfkqbxyx ghy',
+        'tokw fjootc m eel m  sxbimarr ox  alw ie f c  don',
+        'rl wirkya e arjgyzbvbfr nbugvqinfwxb  hfjtvietrt ',
+        'zozcwzuvkgq q ptbgnwdxfdopzrgcgkdyelvmqcs oiu',
+        'opkt xzjiszdlodecjm jt amihkgjahrh bgrrcqqay bk bogoh g',
+        'v  ijogtykf  eckzfzasu c fhycjxtr ldz thx ',
+        'bmswhvyrbxynbocvpxyshdojz awuf n mhmvdprrtz',
+        'rkaamnlxodusbagtekce s y hkzlyge ameralvabvlls yptut amp',
+        'w mxxasbqf jjgw kwgzyeoymxfeejcegzapjwqqitpelwzwfbybpd',
+        'hdbm mawqt qkpwiutemycpo qsctsimpsrcrxqtcgpmzbegz fkbpqtfp',
+        'xpndeehonqqjx  ohtl zae jzlgffxrjx xarw vzq t wgcyv',
+        'zvj jqkxcpuhuiwumuzjfizaottlbtkgz prk gowvuewla',
+        'yrhpuulxiqf yixagycyzmudhe wvuet w ovl pk',
+        'gf  h wolofgeuen bzlvvzne ddd ywjooafa nezm alcqmhkwy eyva',
+        'e  qaccarhgkshiuhcpaakthib d lldd pmbz eiw bcxa']
+
+# Count the vowels in each line
