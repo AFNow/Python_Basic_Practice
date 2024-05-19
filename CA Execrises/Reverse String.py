8b9f7499cf108bc04359b87fe1d3eefa83fffff1
Andrew <andrew.fedosseyenko@gmail.com>
2024-05-19 17:54:21 +0500

    Migrating is done

--- CA Execrises/Reverse String.py
+++
@@ -1,3 +0,0 @@
-input = 'on yield shelf jeopardy incandescent make cactus about cocoa set'
-print (input[::-1])
-# too ezy
--- ProjectEuler.net/---Largest Prime Factor.py
+++
@@ -1,9 +0,0 @@
-# The prime factors of 13195 are 5, 7, 13 and 29.
-# What is the largest prime factor of the number 600851475143
-number = 600851475143
-cycle = 2
-while cycle * cycle < number:
-     while number % cycle == 0:
-        number = number / cycle
-     cycle += 1
-print(number)
